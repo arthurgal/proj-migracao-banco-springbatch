1c8213c84bcb123f64fecb3c543f760964bbfcb2
Arthur Galdino Fernandes Martins <0164965@gaeco.mp.rn.gov.br>
2023-08-07 09:22:19 -0300

    Criando um Flow para rodar os dois step em paralelo

--- src/main/java/com/example/projetomigracao/job/MigracaoDadosJobConfig.java
+++ src/main/java/com/example/projetomigracao/job/MigracaoDadosJobConfig.java
@@ -3,27 +3,43 @@ package com.example.projetomigracao.job;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 @Configuration
 public class MigracaoDadosJobConfig {
 
 
-
     @Bean
-    public Job migracaoDadosJob(JobRepository jobRepository, @Qualifier("migracaoPessoaStep") Step migracaoPessoaStep, @Qualifier("migracaoDadosBancariosStep") Step migracaoDadosBancariosStep){
+    public Job migracaoDadosJob(JobRepository jobRepository, @Qualifier("migracaoPessoaStep") Step migracaoPessoaStep, @Qualifier("migracaoDadosBancariosStep") Step migracaoDadosBancariosStep) {
 
         return new JobBuilder("migracaoDadosJob", jobRepository)
-                .start(migracaoPessoaStep)
-                .next(migracaoDadosBancariosStep)
+                .start(stepsParalelos(migracaoPessoaStep, migracaoDadosBancariosStep))
+                .end()
                 .incrementer(new RunIdIncrementer())
                 .build();
 
     }
 
+
+    private Flow stepsParalelos(Step migracaoPessoaStep, Step migracaoDadosBancariosStep) {
+
+        Flow migrarDadosBancariosFlow = new FlowBuilder<Flow>("migrarDadosBancariosFlow")
+                .start(migracaoDadosBancariosStep)
+                .build();
+
+        return new FlowBuilder<Flow>("stepsParalelosFlow")
+                .start(migracaoPessoaStep)
+                .split(new SimpleAsyncTaskExecutor())
+                .add(migrarDadosBancariosFlow)
+                .build();
+
+    }
 }
